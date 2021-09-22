aa39f4e43cfef7efef7cd56cdbb70677479f21b8
BB Caspian <casfian@gmail.com>
2021-09-22 12:34:44 +0800

    added retrieve data code

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,43 @@
+import 'package:bomba_sqlite/databasehandler.dart';
+import 'package:bomba_sqlite/user.dart';
 import 'package:flutter/material.dart';
 
 //secondary class atau UI
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Databasehandler handler;
+
+  //create data awalan
+  Future<int> createUsers() async {
+    User john = User(
+        name: 'John', age: 24, country: 'Malaysia', email: 'john@gmail.com');
+    User david = User(
+        name: 'David', age: 26, country: 'Malaysia', email: 'david@gmail.com');
+    User abu =
+        User(name: 'Abu', age: 28, country: 'Malaysia', email: 'abu@gmail.com');
+    User ali =
+        User(name: 'Ali', age: 29, country: 'Malaysia', email: 'ali@gmail.com');
+    List<User> listOfUsers = [john, david, abu, ali];
+    return await this.handler.insertUsers(listOfUsers);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.handler = Databasehandler(); //utk On kan Database
+    // this.handler.initialiseDB().whenComplete(() async {
+    //   await this.createUsers(); //create data awalan sekiranya perlu
+    //   setState(() {
+        
+    //   });
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +45,30 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Sqlite Database Demo'),
       ),
-      body: ListView.builder(
-          itemCount: 25,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Data Users'),
-            );
+      body: FutureBuilder(
+          future: this.handler.retrieveUsers(), //sumber data aka data source
+          builder: (context, AsyncSnapshot<List<User>> snapshot) {
+            //check kalau ada data
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data![index].name),
+                      subtitle: Text(snapshot.data![index].email.toString()),
+                    );
+                  });
+            } else {
+              //paparkan data loading...
+              return Center(child: CircularProgressIndicator());
+            }
           }),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              //call function utk add data
-            } ,
-            child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //call function utk add data
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
